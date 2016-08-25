467904fa7ccfbb702a89b5704074c0803d8b75be
Troy Gibb <troy.gibb@gmail.com>
2016-08-25 13:28:44 -0700

    (clean/refactor) took out MeetUp OAuth components

--- client/meetup.js
+++
@@ -1,8 +0,0 @@
-app.controller("MeetupController", ['$scope', 'rmMeetupEventsService', function meetupController($scope, rmMeetupEventsService){
-  $scope.getEvent = function(token, id){
-    rmMeetupEventsService.getByEventId(token, id).then(function(event){
-        $scope.event = event;
-    });
-  }
-  $scope.getEvent()
-}]);
