0f8c97339f5e6a506ba61ace09df8cb578a35d93
Mrinalini Manandhar <mrinalini.manandhar@gmail.com>
2016-08-25 16:14:20 -0700

    (feat) Make angular framework and move skeleton.html to index.html

--- client/app.js
+++ client/app.js
@@ -1,4 +1,20 @@
-angular.module('greenfield', ['greenfield.services', 'ngRoute'])
+angular.module('greenfield', [
+  'greenfield.services', 
+  'greenfield.location',
+  'greenfield.events',
+  'ngRoute'
+])
+.config(function($routeProvider){
+  $routeProvider
+  .when('/', {
+    templateUrl: 'client/location/location.html',
+    controller: 'LocationController'
+  })
+  .when('/events', {
+    templateUrl: 'client/events/events.html',
+    controller: ' '
+  })
+})
 .controller('MainController', ['$scope', function($scope) {
 	$scope.hello = "Hello world!";	
 }]);
