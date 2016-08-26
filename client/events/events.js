847a14b419d9ca4c73468ebb9aadf6ebc83baeca
Mrinalini Manandhar <mrinalini.manandhar@gmail.com>
2016-08-25 17:36:59 -0700

    (feat) Add data request functionality on button submit

--- client/events/events.js
+++ client/events/events.js
@@ -1,4 +1,5 @@
 angular.module('greenfield.events', [])
+<<<<<<< HEAD
 .controller('EventsController', ['$scope', 'Events', function($scope, Events) {
   $scope.data;
   $scope.getEvents = function() {
@@ -15,3 +16,37 @@ angular.module('greenfield.events', [])
 
   };
 }])
+=======
+.controller('EventsController', ['$scope', '$http', function($scope, $http) {
+  $scope.getEvents = function(zip) {
+    $http({
+        method: 'GET',
+        url: '/meetupEvents?zip=' + zip
+      })
+      .then(function (resp) {
+        return resp.data;
+      })
+      .then(function(data) {
+        $scope.usersView = data;
+      })
+      .then(function (resp) {
+        console.log(resp, "THIS DA RESPONSE")
+      });
+  }
+}])
+
+// .factory('Events', function($http, $location) {
+//   var getAll = function(zipcode) {
+//     return $http({
+//       method: 'GET',
+//       url: '/meetupEvents?zip=' + zipcode,
+//     })
+//     .then(function(resp) {
+//       return resp.data;
+//     })
+//   }
+//   return {
+//     getAll: getAll,
+//   }
+// })
+>>>>>>> (feat) Add data request functionality on button submit
--- client/services.js
+++ client/services.js
@@ -20,3 +20,4 @@ angular.module('greenfield.services', [])
     getAll,
   }
 })
+
