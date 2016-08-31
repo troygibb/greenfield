13b5fb3ee4b6db04ce85592f21d1e6705f888ebb
Arnav Aggarwal <arnavaggrwl@gmail.com>
2016-08-31 11:29:31 -0700

    (feat) Improve Facebook functionality to provide more results.
    Instead of manually filtering returned results based on time,
    pass in an 'until' time parameter to the FB api to receive
    more results within the next week.
    
    Minor refactors, plus change range to 10 miles.

--- api_handlers/fb_api.js
+++ api_handlers/fb_api.js
@@ -1,6 +1,6 @@
 const request = require('request');
-const utils = require('./utils');
 const EventSearch = require("facebook-events-by-location-core");
+const { handleUndefined } = require('./utils');
 const {
   GEONAMES_USERNAME,
   FB_APP_ACCESS_TOKEN: accessToken,
@@ -8,32 +8,15 @@ const {
 
 module.exports = {};
 
-const handleUndefined = utils.handleUndefined;
-
-//from http://dejanstojanovic.net/jquery-javascript/2015/september/parsing-date-from-facebook-opengraph-api-response-with-javasscript/
-function parseDateString(dateStr) {  
-    const dateDate = dateStr.split("T")[0];  
-    const dateTime = dateStr.split("T")[1].substring(0, 8);  
-    const dateResult = new Date(Date.UTC(  
-        dateDate.split("-")[0], /* Year */  
-        dateDate.split("-")[1], /* Month */  
-        dateDate.split("-")[2], /* Day */  
-        dateTime.split(":")[0], /* Hour */  
-        dateTime.split(":")[1], /* Minute */  
-        dateTime.split(":")[2]  /* Second*/  
-    ));  
-    return dateResult;  
+function oneUnixWeek() {
+  const now = (new Date()).getTime();
+  const oneWeek = now + 1000 * 60 * 60 * 24 * 7;
+  const oneWeekInUnix = Math.round(oneWeek / 1000);
+  return oneWeekInUnix;
 } 
 
 function formatFbResponse(events, cb) {
-  //filter out events farther away than 1 week
-  const responseJSON = events.filter(event => {
-    const e_date = parseDateString(event.startTime);
-    const today = new Date();
-    const oneWeek = 1000 * 60 * 60 * 24 * 7;
-    return e_date.getTime() - today.getTime() < oneWeek;
-  })
-  .map((event) => {
+  const responseJSON = events.map((event) => {
     return {
       e_title: event.name,
       e_time: event.startTime,
@@ -62,12 +45,9 @@ module.exports.getFbEvents = function(zip, cb) {
   //The FB events api requires a latitutde and longitude.
   //http://www.geonames.org/ is a free api that 
   //converts zip codes to lat/lng:
-  request.get(`http://api.geonames.org/postalCodeSearchJSON?postalcode=${zip}&maxRows=10&username=${GEONAMES_USERNAME}`)
+  request.get(`http://api.geonames.org/postalCodeSearchJSON?postalcode=${ zip }&maxRows=10&username=${ GEONAMES_USERNAME }`)
   .on('data', function(data) {
-    //distance in meters
-    const distance = 20000;
-
-    //information on zip codes is delivered in JSON. One zip code can refer to
+    //information on zip codes is delivered in JSON. One zip can refer to
     //several places across the world so we filter by country code to get
     //only the US result. We access only the latitude/longitude.
     const {lat, lng} = JSON.parse('' + data).postalCodes
@@ -77,8 +57,9 @@ module.exports.getFbEvents = function(zip, cb) {
     const es = new EventSearch({
       lat,
       lng,
-      distance,
       accessToken,
+      distance: 16094,  //10 miles in meters
+      until: oneUnixWeek(),
     });
 
     es.search().then(function (events) {
