92c34e2b69137a74dc6d10fa7c889c9e25f2ea7e
Troy Gibb <troy.gibb@gmail.com>
2016-09-04 20:26:02 -0700

    (feat, cleanup) Category listing/filtering; User saved events functionality; Animation libraries.
    Client - add bootstrap and hover libraries in bower; delete independently added library; add zipcode search bar in the maincontroller nav bar; take out superfluous HTML in index.html; add clickable category listing for all categories; refactor of helper functions in eventList.js into factory called EventOrganizer; create new directive for eventWidgets; create new button for adding to saved user events; create new view userEvents for saved user events.
    
    Bug - facebook api hung app for some reason, commented it out for time being.

---
+++ client/directives/eventWidget.js
@@ -0,0 +1,10 @@
+angular.module('greenfield.directives', [])
+.directive('eventWidget', function(){
+  return {
+  	//Restricts usage only to HTML elements. 
+    restrict: 'E',
+    //To ensure that the widget has access to parent's methods. 
+    transclude: true,
+    templateUrl: 'client/directives/eventWidget.html'
+  };
+});
