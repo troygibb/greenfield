33a4feb0ecc2825b4dedc34af26dd236d767b391
Arnav Aggarwal <arnavaggrwl@gmail.com>
2016-09-06 17:02:20 -0700

    (feat) Sort categories by frequency
    Turn category label into button. Display number of occurences
    on category button. Display in order. NOTE not currently
    accurate - some event counts are +/- about 5 or so from
    actual events that appear. Also - only currently tested for
    funcheapSF events.
    
    Filter out expired events and events later than 1 week.
    
    Refactor most of eventList.js to be cleaner and use primarily
    objects instead of arrays.
    
    Install ngOrderObjectBy using bower - allows ordering by object
    properties in Angular, similar to orderBy for arrays.

--- client/eventList/eventList.js
+++ client/eventList/eventList.js
@@ -1,9 +1,14 @@
-angular.module('greenfield.eventList', [])
-.controller('EventListController', ['$scope', 'Events', 'EventOrganizer', 'EventCache', function($scope,  Events, EventOrganizer, EventCache) {
+angular.module('greenfield.eventList', ['ngOrderObjectBy'])
+
+.constant('_', window._)
+
+.controller('EventListController',
+  ['$scope', 'Events', 'EventOrganizer', 'EventCache', '_',
+  function($scope,  Events, EventOrganizer, EventCache, _) {
   //$scope.img = `assets/meetup-128.png`
 
-  $scope.categories = [];
-  $scope.allEvents = removeDups(Events.savedEvents);
+  $scope.allEvents = removeDuplicateAndExpiredEvents(Events.savedEvents);
+  $scope.categories = addCategories($scope.allEvents);
   $scope.eventsByDate = '';
   $scope.searchText = '';
   $scope.widgetView = false; 
@@ -19,36 +24,69 @@ angular.module('greenfield.eventList', [])
     EventCache.savedEvents.push(eventObject);
   };
 
-  //TODO: Try to implement this on the backend?
-  function removeDups(events){
-    const eventsCopy = [...events];
-    eventsCopy.forEach(function(thisEvent, index) {
-      let cats = thisEvent.e_categories;
-      //Adding to categories set (conditional for null type checking): 
-      if (cats) cats.forEach(cat => {
-        $scope.categories.indexOf(cat) == -1 ? $scope.categories.push(cat) : null;
-      });
-      for(let i = index + 1; i < eventsCopy.length; i++) {
-        if(thisEvent.e_title === eventsCopy[i].e_title && cats) {
-          cats = cats.concat(eventsCopy[i].e_categories);
-        }
+  function oneWeekMS() {
+    const now = (new Date()).getTime();
+    const oneWeek = now + 1000 * 60 * 60 * 24 * 7;
+    return oneWeek;
+  }
 
-        if(cats) {
-          thisEvent.e_categories =
-          cats.filter((cat, index) => index === cats.indexOf(cat));
+  function addCategories(events) {
+    const categories = {};
+
+    events.forEach((thisEvt) => {
+      const cats = thisEvt.e_categories;
+      const now = new Date().getTime();
+      const nextWeek = oneWeekMS();
+      const evtTime = new Date(thisEvt.e_time).getTime();
+      cats && now < evtTime && evtTime < nextWeek &&
+      cats.forEach(cat => {
+        categories[cat] = categories[cat] || {};
+        categories[cat].frequency = categories[cat].frequency + 1 || 1;
+      });
+    });
+
+    return categories;
+  };
+
+  //TODO: Try to implement this on the backend?
+  function removeDuplicateAndExpiredEvents(events){
+    const eventsObj = {};
+
+    //turn eventsObj into a dup-free version of events
+    events.forEach((thisEvent, index) => {
+      //Remove events that have expired or are past 1 week from now
+      const now = new Date().getTime();
+      const nextWeek = oneWeekMS();
+      const evtTime = new Date(thisEvent.e_time).getTime();
+      if(evtTime < now || evtTime > nextWeek) {
+        return;
+      }
+
+      //if categories is not null
+      if(thisEvent.e_categories) {
+        for(let i = index + 1; i < events.length; i++) {
+          const furtherEvent = events[i];
+
+          //if another event has the same title and categories of its own,
+          //concatenate the categories into the 1st event and filter out
+          //duplicate categories
+          if(furtherEvent.e_categories &&
+          thisEvent.e_title === furtherEvent.e_title) {
+            thisEvent.e_categories = thisEvent.e_categories
+            .concat(furtherEvent.e_categories)
+            .filter((cat, index, self) => index === self.indexOf(cat));
+          }
         }
+      }
+
+      //insert the event into eventsObj if it's the first time we've processed an
+      //event with this title
+      if(eventsObj[thisEvent.e_title] === undefined) {
+        eventsObj[thisEvent.e_title] = thisEvent;
       }
     });
-    const evRev = eventsCopy.reverse();
-    return $scope.allEvents = evRev.filter(function(event, index) {
-      for(let i = index + 1; i < eventsCopy.length; i++) {
-        if(event.e_title === evRev[i].e_title && 
-          event.e_time === evRev[i].e_time) {
-          return false
-        };
-      }
-      return true;
-    }).reverse();
+
+    return _.map(eventsObj, event => event);
   };
 
   $scope.getSourceImage = sourceName => Events.getSourceImage(sourceName);
@@ -60,11 +98,6 @@ angular.module('greenfield.eventList', [])
 
   $scope.eventsByDate = EventOrganizer.generateTimeSpan($scope.allEvents, 7);
   $scope.logEvents();
-
-  $scope.distances = [
-    "0.5 miles",
-    "10 miles"
-  ];
 
   $scope.logCats = () => console.log($scope.categories)
   $scope.logDistance = () => console.log($scope.selectedDistance)
